b3273e6bb641c18df64946ffd7aa9f8df2fb750e
victor atencio <victorj.atencio@gmail.com>
2020-08-29 09:59:59 -0400

    first screen ready for desktop

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -3,21 +3,27 @@ import React from "react";
 import CircledAvatar from "../components/circledAvatars/circleAvatar";
 import TitleBar from "../components/titleBar/titleBar";
 
+import "../css/intro.css";
+
 const Intro = () => {
   return (
-    <div>
-      <CircledAvatar withShadow="withShadow" />
-      <h1>
+    <div className="intro__container">
+      <div className="item__avatar">
+        <CircledAvatar withShadow="withShadow" />
+      </div>
+      <h1 className="item__text">
         A peep at some distant orb has power to raise and purify our thoughts
         like a <span className="primary">strain of scared</span> music, or a
         noble picture
       </h1>
 
-      <TitleBar
-        bkgColor="bkg__primary"
-        title="Victor Atencio"
-        supportText="Web Designer & UI Developer"
-      />
+      <div className="item__bar">
+        <TitleBar
+          bkgColor="bkg__primary"
+          title="Victor Atencio"
+          supportText="Web Designer & UI Developer"
+        />
+      </div>
     </div>
   );
 };
